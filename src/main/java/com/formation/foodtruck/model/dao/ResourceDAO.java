bcd786a250961f61f31c4905d2259ae87947122c
rdesfrancois <rdesfrancois@gmail.com>
2014-12-01 13:58:02 +0100

    ResourceDAO

--- src/main/java/com/formation/foodtruck/model/dao/ResourceDAO.java
+++ src/main/java/com/formation/foodtruck/model/dao/ResourceDAO.java
@@ -1,5 +1,13 @@
 package com.formation.foodtruck.model.dao;
 
+import java.util.List;
+
 public interface ResourceDAO extends DAO<Resource>{
 
+	/**
+	 * Find all.
+	 *
+	 * @return the list
+	 */
+	List<Resource> findAll();
 }
